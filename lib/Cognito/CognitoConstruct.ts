9067281ed1eaca5c8b7d17bfb843837318c82a9e
nh51955-hertz-net <michal.kapiczynski@hertz.com>
2021-01-30 18:33:01 +0100

    Fix CognitoConstruct by adding resource server definition

--- lib/Cognito/CognitoConstruct.ts
+++ lib/Cognito/CognitoConstruct.ts
@@ -1,15 +1,28 @@
 import {CfnOutput, Construct} from '@aws-cdk/core';
-import {OAuthScope, UserPool, UserPoolClient, UserPoolDomain} from "@aws-cdk/aws-cognito";
+import {CfnUserPoolResourceServer, OAuthScope, UserPool, UserPoolClient, UserPoolDomain} from "@aws-cdk/aws-cognito";
 
 export class CognitoConstruct extends Construct {
     public static readonly USER_POOL_ID = 'UserManagerUserPool';
     public static readonly USER_POOL_CLIENT_ID = 'user-manager-client';
     public static readonly USER_POOL_DOMAIN_ID = 'user-manager-user-pool-domain';
+    public static readonly USER_POOL_RESOURCE_SERVER_ID = 'https://resource-server.com';
     public readonly userPoolArn: string;
 
     constructor(scope: Construct) {
         super(scope, CognitoConstruct.USER_POOL_ID);
         const cognitoUserPool = new UserPool(this, CognitoConstruct.USER_POOL_ID, {})
+
+        new CfnUserPoolResourceServer(this, "dev-userpool-resource-server", {
+            identifier: CognitoConstruct.USER_POOL_RESOURCE_SERVER_ID,
+            name: "userpool-resource-server",
+            userPoolId: cognitoUserPool.userPoolId,
+            scopes: [
+                {
+                    scopeDescription: "Perform client operations",
+                    scopeName: "user-manager-client",
+                },
+            ],
+        });
 
         new UserPoolClient(this, CognitoConstruct.USER_POOL_CLIENT_ID, {
             userPool: cognitoUserPool,
@@ -18,7 +31,7 @@ export class CognitoConstruct extends Construct {
                 flows: {
                     clientCredentials: true
                 },
-                scopes: [OAuthScope.custom('https://my-secure-api.example.com/admin')]
+                scopes: [OAuthScope.custom(`${CognitoConstruct.USER_POOL_RESOURCE_SERVER_ID}/user-manager-client`)],
             },
         })
 
