86a87ff2267c32bf2b7929da6f55a8aa6d3998e7
nh51955-hertz-net <michal.kapiczynski@hertz.com>
2021-04-29 13:27:57 +0200

    Add account and region to stack properties

--- lib/user-manager-serverless-stack.ts
+++ lib/user-manager-serverless-stack.ts
@@ -8,7 +8,13 @@ export class UserManagerServerlessStack extends cdk.Stack {
     private static readonly API_ID = 'UserManagerApi';
 
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+        super(scope, id, {
+            ...props,
+            env: {
+                account: process.env.CDK_DEFAULT_ACCOUNT,
+                region: process.env.CDK_DEFAULT_REGION
+            }
+        });
         const cognitoConstruct = new CognitoConstruct(this)
         const usersDynamoDbTable = new UsersDynamoDbTable(this);
         const lambdaConstruct = new LambdaConstruct(this, usersDynamoDbTable);
